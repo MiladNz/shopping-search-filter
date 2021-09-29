5082d9a24f58e54e0bdbfafe03cc7631cab0c6e4
MiladNz <milad.n91@gmail.com>
2021-09-29 13:44:57 +0330

    commit on master

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //http://localhost:3000/items
-
+const searchInput = document.querySelector("#search");
+const productsDOM = document.querySelector(".products-center");
 let allProductsdata = [];
-
+const filters = {
+    searchItems: "",
+};
 document.addEventListener("DOMContentLoaded" , ()=>{
     //console.log("loaded... ");
     axios
@@ -9,6 +12,37 @@ document.addEventListener("DOMContentLoaded" , ()=>{
     .then((res) => {
         allProductsdata = res.data;
         //renedr products on DOM :
+        renderProducts(res.data,filters);
     })
     .catch((err) => console.log(err));
+});
+
+function renderProducts(_products,_filters){
+    const filteredProducts = _products.filter( p => {
+        return p.title.toLowerCase().includes(_filters.searchItems.toLowerCase());
+    });
+    console.log(filteredProducts);
+    //render to DOM
+    filteredProducts.forEach((item,index) => {
+        //create
+        //content
+        const productsDiv = document.createElement("div");
+        productsDiv.classList.add("product");
+        productsDiv.innerHTML = 
+        `<div class="img-container">
+            <img src=${item.image} alt="p-${index}"/>
+          </div>
+          <div class="product-desc">
+            <p class="product-price">${item.price} $</p>
+            <p class="product-title">${item.title}</p>
+          </div>`;
+          //append to DOM
+          productsDOM.appendChild(productsDiv);
+        });
+}
+
+searchInput.addEventListener("input" ,(e)=>{
+    console.log(e.target.value);
+    filters.searchItems = e.target.value;
+    renderProducts(allProductsdata,filters);
 });
